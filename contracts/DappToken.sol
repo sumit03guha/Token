5d23560533bd9e3c2454a488644d731ddcaf9111
Sumit Guha <sumit03guha@gmail.com>
2021-08-03 16:09:57 +0530

    Smoke Test

---
+++ contracts/DappToken.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.4;
+
+contract DappToken {
+    
+    uint public totalSupply;
+    
+    constructor() {
+        totalSupply = 1000000;
+    }
+
+}
---
+++ test/DappTokenTest.js
@@ -0,0 +1,18 @@
+const DappToken = artifacts.require('./DappToken');
+
+contract('DappToken', function (accounts) {
+  it('sets the total supply upon deployment', function () {
+    return DappToken.deployed()
+      .then(function (instance) {
+        tokenInstance = instance;
+        return tokenInstance.totalSupply();
+      })
+      .then(function (totalSupply) {
+        assert.equal(
+          totalSupply.toNumber(),
+          1000000,
+          'sets the total supply to 1,000,000'
+        );
+      });
+  });
+});
