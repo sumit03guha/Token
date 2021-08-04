7112f12dfb309b6a78b1e6ac3e903963ee130aec
Sumit Guha <sumit03guha@gmail.com>
2021-08-04 19:36:17 +0530

    added transfer event

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -5,15 +5,29 @@ pragma solidity >=0.8.4;
 contract DappToken {
     
     uint public totalSupply;
-    mapping(address => uint) public balanceOf;
     string public name = 'DApp Token';
     string public symbol = 'DAPP';
     string public standard = 'DApp Token v1.0';
+    mapping(address => uint) public balanceOf;
+    
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint _value
+        );
     
     constructor(uint _initialSupply) {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
     }
     
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        
+        emit Transfer(msg.sender , _to , _value);
+        return true;
+    }
     
 }
--- test/DappTokenTest.js
+++ test/DappTokenTest.js
@@ -28,5 +28,34 @@ contract('DappToken', function (accounts) {
       const standard = await tokenInstance.standard();
       assert.equal(standard, 'DApp Token v1.0');
     });
+
+    it('transfers token ownership', async () => {
+      const receipt = await tokenInstance.transfer(accounts[1], 10, {
+        from: accounts[0],
+      });
+      assert.equal(receipt.logs.length, 1, 'triggers one event');
+      assert.equal(
+        receipt.logs[0].event,
+        'Transfer',
+        'should be the "Transfer" event'
+      );
+      assert.equal(
+        receipt.logs[0].args._from,
+        accounts[0],
+        'logs the account the tokens are transferred from'
+      );
+      assert.equal(
+        receipt.logs[0].args._to,
+        accounts[1],
+        'logs the account the tokens are transferred to'
+      );
+      assert.equal(receipt.logs[0].args._value, 10, 'logs the transfer amount');
+      const balance = await tokenInstance.balanceOf(accounts[1]);
+      assert.equal(balance, 10);
+      const success = await tokenInstance.transfer.call(accounts[1], 10, {
+        from: accounts[0],
+      });
+      assert.equal(success, true);
+    });
   });
 });
